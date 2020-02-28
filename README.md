1e0b9f0fe6aa415aeeab2d829c798f1b0bdae483
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-02-28 09:47:26 -0700

    Style navbar with styled-components

--- README.md
+++ README.md
@@ -97,9 +97,9 @@ _Display Data from a Server API_
 _Add a Router to this application using
 [React Router](https://reacttraining.com/react-router/web/guides/quick-start)._
 
-- [ ] Hook up the Welcome page(Home page) and a Characters page with React
+- [x] Hook up the Welcome page(Home page) and a Characters page with React
       Router.
-- [ ] Use a styling or component library for part of or all of your application.
+- [x] Use a styling or component library for part of or all of your application.
       (Pick at least 1 of:
       [s](https://react-bootstrap.github.io/)tyled-components or Reactstrap).
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
-export default function App() {
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  padding: 1%;
+
+  &.active {
+    border: solid black 1px;
+  }
+`;
+
+export default function Navbar() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/characters">Characters</NavLink>
-    </nav>
+    <Nav>
+      <StyledNavLink exact to="/">Home</StyledNavLink>
+      <StyledNavLink to="/characters">Characters</StyledNavLink>
+    </Nav>
   );
 }
